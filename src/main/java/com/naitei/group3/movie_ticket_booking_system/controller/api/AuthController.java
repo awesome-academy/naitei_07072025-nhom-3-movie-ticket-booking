d0c6148e09e9fd741ac132fafadf2339df1fcf65
Thanh Tinh Nguyen <ngthanhtinh801@gmail.com>
2025-08-21 09:15:22 +0700

    send verify email

--- src/main/java/com/naitei/group3/movie_ticket_booking_system/controller/api/AuthController.java
+++ src/main/java/com/naitei/group3/movie_ticket_booking_system/controller/api/AuthController.java
@@ -1,9 +1,13 @@
 package com.naitei.group3.movie_ticket_booking_system.controller.api;
 
 import com.naitei.group3.movie_ticket_booking_system.dto.request.RegisterRequestDTO;
+import com.naitei.group3.movie_ticket_booking_system.dto.response.BaseApiResponse;
 import com.naitei.group3.movie_ticket_booking_system.dto.response.UserResponseDTO;
 import com.naitei.group3.movie_ticket_booking_system.enums.RoleType;
+import com.naitei.group3.movie_ticket_booking_system.utils.MessageUtil;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.naitei.group3.movie_ticket_booking_system.entity.User;
@@ -11,23 +15,31 @@ import com.naitei.group3.movie_ticket_booking_system.service.UserService;
 
 @RestController("apiAuthController")
 @RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
-  private final UserService userService;
+    private final UserService userService;
+    private final MessageUtil messageUtil;
 
-  public AuthController(UserService userService) {
-    this.userService = userService;
-  }
+    @PostMapping("/register")
+    public ResponseEntity<BaseApiResponse<UserResponseDTO>> register(@Valid @RequestBody RegisterRequestDTO request) {
+        User savedUser = userService.registerUser(request, RoleType.USER);
 
-  @PostMapping("/register")
-  public ResponseEntity<UserResponseDTO> register(@Valid @RequestBody RegisterRequestDTO request) {
-    User savedUser = userService.registerUser(request, RoleType.USER);
+        UserResponseDTO responseDTO = new UserResponseDTO(
+                savedUser.getId(),
+                savedUser.getName(),
+                savedUser.getRole().getName()
+        );
 
-    UserResponseDTO responseDTO = new UserResponseDTO(
-        savedUser.getId(),
-        savedUser.getName(),
-        savedUser.getRole().getName()
-    );
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                new BaseApiResponse<>(HttpStatus.CREATED.value(), responseDTO)
+        );
+    }
 
-    return ResponseEntity.ok(responseDTO);
-  }
+    @GetMapping("/verify")
+    public ResponseEntity<BaseApiResponse<String>> verifyEmail(@RequestParam("token") String token) {
+        userService.verifyEmail(token);
+        return ResponseEntity.ok(
+                new BaseApiResponse<>(HttpStatus.OK.value(), messageUtil.getMessage("email.verified"))
+        );
+    }
 }
