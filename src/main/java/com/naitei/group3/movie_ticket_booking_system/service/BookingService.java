1ca950f2384917943c335e5663f883d666520bd8
tranvananhanhanh <khuctieuho@gmail.com>
2025-08-21 14:43:56 +0700

    ver 1 booking
    ver1 booking fix
    
    api booking
    
    ver 1.1 booking
    
    ver2 booking
    
    ver 2.2 booking
    
    fix auto cleanbooking
    
    ver 1 cancel booking
    
    ver 1.2 cancel booking
    
    fix cancelt
    
    ver 2.1 canceltn
    
    update cancel

--- src/main/java/com/naitei/group3/movie_ticket_booking_system/service/BookingService.java
+++ src/main/java/com/naitei/group3/movie_ticket_booking_system/service/BookingService.java
@@ -11,12 +11,14 @@ import com.naitei.group3.movie_ticket_booking_system.entity.Booking;
 public interface BookingService {
     BaseApiResponse<BookingDTO> createBooking(Long userId, BookingRequestDTO request);
 
-    void releaseExpiredBookings();
+    BaseApiResponse<BookingDTO> getBookingById(Long bookingId);
 
-    BaseApiResponse<BookingDTO> getBookingById(Long id);
     
     BaseApiResponse<List<BookingHistoryDTO>> getBookingHistory(Long userId);
 
 
 
+    BaseApiResponse<Void> cancelBooking(Long bookingId, Long userId);
+
+    void releaseExpiredBookings();
 }
