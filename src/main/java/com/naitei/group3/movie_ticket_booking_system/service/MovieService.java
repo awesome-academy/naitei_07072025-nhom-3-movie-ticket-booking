b7924f0dc5a655b1f5bb971a41673abac0048848
Thanh Tinh Nguyen <ngthanhtinh801@gmail.com>
2025-08-24 20:42:09 +0700

    task search phim cho api user

--- src/main/java/com/naitei/group3/movie_ticket_booking_system/service/MovieService.java
+++ src/main/java/com/naitei/group3/movie_ticket_booking_system/service/MovieService.java
@@ -1,11 +1,10 @@
 package com.naitei.group3.movie_ticket_booking_system.service;
 
+import com.naitei.group3.movie_ticket_booking_system.dto.request.MovieSearchApiRequest;
 import com.naitei.group3.movie_ticket_booking_system.dto.response.MovieDTO;
 import com.naitei.group3.movie_ticket_booking_system.entity.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 public interface MovieService {
 
@@ -14,4 +13,5 @@ public interface MovieService {
     MovieDTO getMovieById(Long id);
     Page<Movie> getNowShowingMovies(Pageable pageable);
     void delete(Long id);
+    Page<MovieDTO> searchMovies(MovieSearchApiRequest req, Pageable pageable);
 }
