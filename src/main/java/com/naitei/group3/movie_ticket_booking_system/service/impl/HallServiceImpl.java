e903bf75d171fd043a8ebabfbffe9804db2dab89
MinhTuan <tuanmt2912@gmail.com>
2025-08-28 15:53:35 +0700

    [Task][92370]Feat(admin): edit name hall
    [Task][92370]Feat(admin): edit name hall

--- src/main/java/com/naitei/group3/movie_ticket_booking_system/service/impl/HallServiceImpl.java
+++ src/main/java/com/naitei/group3/movie_ticket_booking_system/service/impl/HallServiceImpl.java
@@ -10,6 +10,7 @@ import com.naitei.group3.movie_ticket_booking_system.repository.CinemaRepository
 import com.naitei.group3.movie_ticket_booking_system.repository.HallRepository;
 import com.naitei.group3.movie_ticket_booking_system.service.HallService;
 import lombok.RequiredArgsConstructor;
+import com.naitei.group3.movie_ticket_booking_system.exception.BadRequestException;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
@@ -48,5 +49,46 @@ public class HallServiceImpl implements HallService {
         hallRepository.save(hall);
     }
 
+    @Override
+    public Hall getHallById(Long hallId) {
+        return hallRepository.findById(hallId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                                messageSource.getMessage(
+                                        "hall.notfound",
+                                        new Object[]{hallId},
+                                        LocaleContextHolder.getLocale()
+                                )
+                        )
+                );
+    }
 
+    @Override
+    @Transactional
+    public Hall updateHallName(Long hallId, String newName) {
+        Hall hall = hallRepository.findById(hallId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        messageSource.getMessage("hall.notfound", new Object[]{hallId}, LocaleContextHolder.getLocale())
+                ));
+
+        // Phải điền "Hall + số phòng"
+        if (!newName.matches("^Hall\\s*\\d+$")) {
+            throw new BadRequestException(
+                    messageSource.getMessage("hall.invalidName", new Object[]{newName}, LocaleContextHolder.getLocale())
+            );
+        }
+
+        Cinema cinema = hall.getCinema();
+
+        // Kiểm tra trùng tên phòng khác trong cùng 1 cinema
+        if (hallRepository.existsByCinemaAndName(cinema, newName) && !hall.getName().equals(newName)) {
+            throw new BadRequestException(
+                    messageSource.getMessage("hall.duplicateName",
+                            new Object[]{newName, cinema.getId()},
+                            LocaleContextHolder.getLocale())
+            );
+        }
+
+        hall.setName(newName);
+        return hallRepository.save(hall);
+    }
 }
